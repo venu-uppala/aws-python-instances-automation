0d38553daa7883928c52ac567a24b56ff9e3d611
Venu Uppala <venuhdptrng@gmail.com>
2020-01-09 17:56:37 +1100

    Added project filter to list, stop and start ec2 instances

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # aws-python-instances-automation
 Automate list aws ec2 instances, volumes and snapshots, start and stop instances, and take snapshot of volumes
 
-`pipenv run python shotty/shotty.py <command>`
+`pipenv run python shotty/shotty.py <command> <option>`
 
 *Command* can be list-instances or stop or start
+*option* is --project
--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,15 +4,21 @@ import click
 session = boto3.Session(profile_name='pythonautomation')
 ec2 = session.resource('ec2')
 
+def filter_instances(project):
+    filters = [{'Name':'tag:Project', 'Values':[project]}]
+    return ec2.instances.filter(Filters=filters)
+
 @click.group()
 def instances():
     "Operations on Ec2 instances"
 
 @instances.command("list-instances")
-def list_instances():
+@click.option('--project', required=True)
+def list_instances(project):
     "List EC2 instances"
 
-    for i in ec2.instances.all():
+    instances = filter_instances(project);
+    for i in instances:
 
         print(','.join((
         i.id,
@@ -21,13 +27,16 @@ def list_instances():
         i.placement['AvailabilityZone'],
         i.public_dns_name))
         )
+
     return
 
 @instances.command("stop")
-def stop_instances():
+@click.option('--project', required=True)
+def stop_instances(project):
     "Stop EC2 instances"
 
-    for i in ec2.instances.all():
+    instances = filter_instances(project);
+    for i in instances:
 
         print("Stopping {0} instance".format(i.id))
         i.stop()
@@ -35,10 +44,12 @@ def stop_instances():
     return
 
 @instances.command("start")
-def start_instances():
+@click.option('--project', required=True)
+def start_instances(project):
     "Start EC2 instances"
 
-    for i in ec2.instances.all():
+    instances = filter_instances(project);
+    for i in instances:
 
         print("Starting {0} instance".format(i.id))
         i.start()
